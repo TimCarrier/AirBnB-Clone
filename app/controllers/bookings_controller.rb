ab2503ab9cda4771d6f48a1b297cc07ab625310c
Thanh Truc Pham <truc.tpham97@gmail.com>
2024-05-28 14:39:52 +0200

    controller methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,36 @@
 class BookingsController < ApplicationController
+  before_action :set_user, only: [:index]
+  before_action :set_booking, only: [:show]
   def index
+    @bookings = @user.bookings
   end
 
   def show
   end
 
+  def new
+    @booking = Booking.new
+  end
+
   def create
+    @booking = Booking.new(booking_params)
   end
 
   def destroy
+    flash[:notice] = "User was successfully destroyed." if @user.destroy
+    redirect_to root_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date)
   end
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,23 +1,47 @@
 class DogsController < ApplicationController
-
+  before_action :set_dog, only: [:show, :edit, :update, :delete]
   def index
+    @dogs = Dog.all
   end
 
   def show
   end
 
   def new
+    @dog = Dog.new
   end
 
   def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new, status: 422
+    end
   end
 
   def edit
   end
 
   def update
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog)
+    else
+      render :new, status: 422
+    end
   end
 
   def destroy
+    flash[:notice] = "Post was successfully deleted" if @dog.destroy
+    redirect_to dogs_path
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :age, :disponibility, :race, :description, :address)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
   end
 end
