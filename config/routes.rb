17c92d6e82d1d830f68b2ded9a5eacf3de7a2787
robin chaux <robinvchaux@gmail.com>
2024-05-28 13:53:38 +0200

    Updated routes, root to dogs#index, updated models for dependant: :destroy on has_many not on belongs_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "dogs#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :dogs
-  resources :bookings, only: [:index, :show, :create, :destroy]
   resources :users, only: [:show, :edit, :update, :destroy]
+  resources :dogs, except: [:index] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :destroy]
 end
